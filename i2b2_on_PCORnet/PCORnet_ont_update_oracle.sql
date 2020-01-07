04dd5526736b39551984f1b3b9ee109f91225d75
nihr43 <nathanihensel@gmail.com>
2020-01-07 15:58:17 -0600

    i2b2_on_PCORnet: terminate statements

--- i2b2_on_PCORnet/PCORnet_ont_update_oracle.sql
+++ i2b2_on_PCORnet/PCORnet_ont_update_oracle.sql
@@ -1,18 +1,18 @@
 DEFINE metadata_schema = 'DUAL' ;
 
 update &metadata_schema..pcornet_proc
-  set c_facttablecolumn='MULTIFACT_PROCEDURE_VIEW.'||c_facttablecolumn
+  set c_facttablecolumn='MULTIFACT_PROCEDURE_VIEW.'||c_facttablecolumn ;
 update &metadata_schema..pcornet_diag
-  set c_facttablecolumn='MULTIFACT_DIAGNOSIS_VIEW.'||c_facttablecolumn
+  set c_facttablecolumn='MULTIFACT_DIAGNOSIS_VIEW.'||c_facttablecolumn ;
 update &metadata_schema..pcornet_med
-  set c_facttablecolumn='MULTIFACT_PRESCRIBING_VIEW.'||c_facttablecolumn
+  set c_facttablecolumn='MULTIFACT_PRESCRIBING_VIEW.'||c_facttablecolumn ;
 update &metadata_schema..pcornet_lab
-  set c_facttablecolumn='MULTIFACT_LABRESULTS_VIEW.'||c_facttablecolumn
+  set c_facttablecolumn='MULTIFACT_LABRESULTS_VIEW.'||c_facttablecolumn ;
 update &metadata_schema..pcornet_vital
-  set c_facttablecolumn='MULTIFACT_VITAL_VIEW.'||c_facttablecolumn
+  set c_facttablecolumn='MULTIFACT_VITAL_VIEW.'||c_facttablecolumn ;
 -- Undo the change on diagnosis for modifiers to enable diag modifiers
 update &metadata_schema..pcornet_diag
-  set c_facttablecolumn='modifier_cd' where c_facttablecolumn like '%.modifier_cd'
+  set c_facttablecolumn='modifier_cd' where c_facttablecolumn like '%.modifier_cd' ;
 
 -- Undo a default ontology change inserted to match to demodata in i2b2
 UPDATE "i2b2metadata"."pcornet_diag"
