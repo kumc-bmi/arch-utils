f6745e48d06d1d0ec098f2876bb205f289e2377b
nihr43 <nathanihensel@gmail.com>
2020-01-23 12:07:02 -0600

    capitalization nit

--- i2b2_on_PCORnet/PCORnet_star_view_oracle.sql
+++ i2b2_on_PCORnet/PCORnet_star_view_oracle.sql
@@ -1,8 +1,9 @@
 DEFINE source_schema = &1;
 DEFINE target_schema = &2;
 
+-- select grant must be in place for views
 GRANT SELECT on &source_schema..diagnosis to &target_schema ;
-GRANT SELECT on &source_schema..ENROLLMENT to &target_schema ;
+GRANT SELECT on &source_schema..enrollment to &target_schema ;
 GRANT SELECT on &source_schema..lab_result_cm to &target_schema ;
 GRANT SELECT on &source_schema..prescribing to &target_schema ;
 GRANT SELECT on &source_schema..procedures to &target_schema ;
