b1e8340daae04169a70899ec46eb3196562ac2e7
nihr43 <nathanihensel@gmail.com>
2020-01-29 09:44:07 -0600

    add truncate_metadata

---
+++ i2b2_on_PCORnet/truncate_metadata_ora.sql
@@ -0,0 +1,17 @@
+DEFINE i2b2_metadata = &1;
+
+drop table &i2b2_metadata..BIRN ;
+drop table &i2b2_metadata..CUSTOM_META ;
+drop table &i2b2_metadata..I2B2 ;
+drop table &i2b2_metadata..ICD10_ICD9 ;
+drop table &i2b2_metadata..ONT_PROCESS_STATUS ;
+drop table &i2b2_metadata..PCORNET_DEMO ;
+drop table &i2b2_metadata..PCORNET_DIAG ;
+drop table &i2b2_metadata..PCORNET_ENC ;
+drop table &i2b2_metadata..PCORNET_ENROLL ;
+drop table &i2b2_metadata..PCORNET_LAB ;
+drop table &i2b2_metadata..PCORNET_MED ;
+drop table &i2b2_metadata..PCORNET_PROC ;
+drop table &i2b2_metadata..PCORNET_VITAL ;
+drop table &i2b2_metadata..PHI ;
+drop table &i2b2_metadata..SCHEMES ;
