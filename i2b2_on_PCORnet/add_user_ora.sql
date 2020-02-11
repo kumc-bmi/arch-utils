186b611b4de09f2b2399312cf278a8921490f7b2
nihr43 <nathanihensel@gmail.com>
2020-02-11 16:47:09 -0600

    create add_user_ora.sql

---
+++ i2b2_on_PCORnet/add_user_ora.sql
@@ -0,0 +1,42 @@
+define pm_schema = &1;
+define user_id = &2;
+
+whenever sqlerror continue;
+delete from i2b2pm.pm_project_user_roles where user_id = '&user_id' and project_id ='pcornet_cdm';
+
+whenever sqlerror exit;
+insert into &pm_schema..pm_project_user_roles (
+  project_id,
+  user_id,
+  user_role_cd,
+  status_cd
+) values (
+  'pcornet_cdm',
+  '&user_id',
+  'USER',
+  'A'
+);
+
+insert into &pm_schema..pm_project_user_roles (
+  project_id,
+  user_id,
+  user_role_cd,
+  status_cd
+) values (
+  'pcornet_cdm',
+  '&user_id',
+  'DATA_AGG',
+  'A'
+);
+
+insert into &pm_schema..pm_project_user_roles (
+  project_id,
+  user_id,
+  user_role_cd,
+  status_cd
+) values (
+  'pcornet_cdm',
+  '&user_id',
+  'DATA_DEID',
+  'A'
+);
