141d04a97518d07c30da7e61aebe056bba18abfd
nihr43 <nathanihensel@gmail.com>
2020-02-14 14:06:35 -0600

    more robust plsql

--- i2b2_on_PCORnet/truncate_schema_ora.sql
+++ i2b2_on_PCORnet/truncate_schema_ora.sql
@@ -1,36 +1,28 @@
-DEFINE i2b2_metadata = &1;
-
-declare
-begin
-  for indices in ( select index_name from user_indexes ) loop
-    begin
-      execute immediate ( 'drop index ' ||indices.index_name );
-    end;
-  end loop;
-end;
+-- warning, this is very destructive
 
 declare
 begin
   for tables in ( select table_name from user_tables ) loop
     begin
-      execute immediate ( 'drop table ' ||tables.table_name );
+      execute immediate ( 'drop table ' ||tables.table_name|| ' cascade constraints' );
+    end;
+  end loop;
+
+  for indices in ( select index_name from user_indexes where index_type = 'NORMAL' ) loop
+    begin
+      execute immediate ( 'drop index ' ||indices.index_name );
+    end;
+  end loop;
+
+  for myviews in ( select view_name from user_views ) loop
+    begin
+      execute immediate ( 'drop view ' ||myviews.view_name );
+    end;
+  end loop;
+
+  for myprocedures in ( select object_name from user_procedures ) loop
+    begin
+      execute immediate ( 'drop procedure ' ||myprocedures.object_name );
     end;
   end loop;
 end;
-
-
-drop table &i2b2_metadata..BIRN ;
-drop table &i2b2_metadata..CUSTOM_META ;
-drop table &i2b2_metadata..I2B2 ;
-drop table &i2b2_metadata..ICD10_ICD9 ;
-drop table &i2b2_metadata..ONT_PROCESS_STATUS ;
-drop table &i2b2_metadata..PCORNET_DEMO ;
-drop table &i2b2_metadata..PCORNET_DIAG ;
-drop table &i2b2_metadata..PCORNET_ENC ;
-drop table &i2b2_metadata..PCORNET_ENROLL ;
-drop table &i2b2_metadata..PCORNET_LAB ;
-drop table &i2b2_metadata..PCORNET_MED ;
-drop table &i2b2_metadata..PCORNET_PROC ;
-drop table &i2b2_metadata..PCORNET_VITAL ;
-drop table &i2b2_metadata..PHI ;
-drop table &i2b2_metadata..SCHEMES ;
