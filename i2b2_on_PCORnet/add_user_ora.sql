ade6362629bae4476c96c0f332cb74c2dfd6217d
nihr43 <nathanihensel@gmail.com>
2020-02-14 12:57:52 -0600

    change project_id

--- i2b2_on_PCORnet/add_user_ora.sql
+++ i2b2_on_PCORnet/add_user_ora.sql
@@ -2,7 +2,9 @@ define pm_schema = &1;
 define user_id = &2;
 
 whenever sqlerror continue;
-delete from i2b2pm.pm_project_user_roles where user_id = '&user_id' and project_id ='pcornet_cdm';
+delete from i2b2pm.pm_project_user_roles
+  where user_id = '&user_id'
+  and project_id = 'PCORI';
 
 whenever sqlerror exit sql.sqlcode;
 insert into &pm_schema..pm_project_user_roles (
@@ -11,7 +13,7 @@ insert into &pm_schema..pm_project_user_roles (
   user_role_cd,
   status_cd
 ) values (
-  'pcornet_cdm',
+  'PCORI',
   '&user_id',
   'USER',
   'A'
@@ -23,7 +25,7 @@ insert into &pm_schema..pm_project_user_roles (
   user_role_cd,
   status_cd
 ) values (
-  'pcornet_cdm',
+  'PCORI',
   '&user_id',
   'DATA_AGG',
   'A'
@@ -35,7 +37,7 @@ insert into &pm_schema..pm_project_user_roles (
   user_role_cd,
   status_cd
 ) values (
-  'pcornet_cdm',
+  'PCORI',
   '&user_id',
   'DATA_DEID',
   'A'
