8213a9eaca4448bf3e9977820475245d05de1172
nihr43 <nathanihensel@gmail.com>
2020-02-14 14:05:27 -0600

    rename truncate script

--- i2b2_on_PCORnet/truncate_schema_ora.sql
+++ i2b2_on_PCORnet/truncate_schema_ora.sql
@@ -1,5 +1,24 @@
 DEFINE i2b2_metadata = &1;
 
+declare
+begin
+  for indices in ( select index_name from user_indexes ) loop
+    begin
+      execute immediate ( 'drop index ' ||indices.index_name );
+    end;
+  end loop;
+end;
+
+declare
+begin
+  for tables in ( select table_name from user_tables ) loop
+    begin
+      execute immediate ( 'drop table ' ||tables.table_name );
+    end;
+  end loop;
+end;
+
+
 drop table &i2b2_metadata..BIRN ;
 drop table &i2b2_metadata..CUSTOM_META ;
 drop table &i2b2_metadata..I2B2 ;
