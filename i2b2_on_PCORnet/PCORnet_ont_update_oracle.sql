191f28d8942764039c30d74f203256094ed6d462
nihr43 <nathanihensel@gmail.com>
2020-02-17 14:25:12 -0600

    leave patient and visit dimension alone

--- i2b2_on_PCORnet/PCORnet_ont_update_oracle.sql
+++ i2b2_on_PCORnet/PCORnet_ont_update_oracle.sql
@@ -37,3 +37,22 @@ UPDATE &metadata_schema..pcornet_diag
     AND c_fullname='\PCORI_MOD\PDX\X\'
     AND c_name='Unable to classify'
     AND c_synonym_cd='N' ;
+
+-- &i2b2_metadata `C_TABLENAME`s need to point at tables in &i2b2_data
+--UPDATE &metadata_schema..PCORNET_DEMO
+--  set C_TABLENAME = 'MULTIFACT_PATIENT_DIMENSION_VIEW'
+--  where regexp_like(C_TABLENAME, '^patient_dimension$', 'i');
+
+-- PCORNET_DIAG taken care of already
+
+--UPDATE &metadata_schema..PCORNET_ENC
+--  set C_TABLENAME = 'MULTIFACT_VISIT_DIMENSION_VIEW'
+--  where regexp_like(C_TABLENAME, '^visit_dimension$', 'i');
+
+--UPDATE &metadata_schema..PCORNET_ENROLL
+--  set C_TABLENAME = 'MULTIFACT_VISIT_DIMENSION_VIEW'
+--  where regexp_like(C_TABLENAME, '^visit_dimension$', 'i');
+
+-- PCORNET_LAB taken care of already
+-- PCORNET_MED taken care of already
+-- PCORNET_PROC taken care of already
