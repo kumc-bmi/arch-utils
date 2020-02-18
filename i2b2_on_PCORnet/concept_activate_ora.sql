eb646800a7c8fed8a6744d5e37dfa6d817029141
nihr43 <nathanihensel@gmail.com>
2020-02-18 11:03:26 -0600

    activate demographic concepts

--- i2b2_on_PCORnet/concept_activate_ora.sql
+++ i2b2_on_PCORnet/concept_activate_ora.sql
@@ -23,6 +23,34 @@ insert into &i2b2_data..CONCEPT_DIMENSION(
 )
 select distinct
   ib.c_basecode,
+  ib.c_fullname,
+  max(ib.c_name),
+  update_date,
+  download_date,
+  sysdate,
+  sourcesystem_cd
+from ( select * from &i2b2_metadata..PCORNET_DEMO  ) ib
+where ib.c_basecode is not null
+  and ib.c_dimcode is not null
+group by ib.c_basecode,
+  ib.c_fullname,
+  update_date,
+  download_date,
+  sysdate,
+  sourcesystem_cd
+;
+
+insert into &i2b2_data..CONCEPT_DIMENSION(
+  concept_cd,
+  concept_path,
+  name_char,
+  update_date,
+  download_date,
+  import_date,
+  sourcesystem_cd
+)
+select distinct
+  ib.c_basecode,
   ib.c_dimcode,
   max(ib.c_name),
   update_date,
