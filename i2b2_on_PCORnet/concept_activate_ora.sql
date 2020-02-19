5b019f1bf8ee7bb6d38636afc5f809e4fc25e55f
nihr43 <nathanihensel@gmail.com>
2020-02-19 12:11:21 -0600

    resolve primary key conflicts

--- i2b2_on_PCORnet/concept_activate_ora.sql
+++ i2b2_on_PCORnet/concept_activate_ora.sql
@@ -152,36 +152,6 @@ group by
   sourcesystem_cd
 ;
 
--- PK conflict
-insert into &i2b2_data..CONCEPT_DIMENSION(
-  concept_cd, 
-  concept_path, 
-  name_char,
-  update_date, 
-  download_date, 
-  import_date, 
-  sourcesystem_cd
-)
-select distinct 
-  ib.c_basecode,
-  ib.c_dimcode, 
-  max(ib.c_name), 
-  update_date, 
-  download_date, 
-  sysdate, 
-  sourcesystem_cd
-from (select * from &i2b2_metadata..PCORNET_LAB) ib
-where ib.c_basecode is not null
-group by
-  ib.c_basecode,
-  ib.c_dimcode,
-  update_date,
-  download_date,
-  sysdate,
-  sourcesystem_cd
-;
-
--- PK conflict
 insert into &i2b2_data..CONCEPT_DIMENSION(
   concept_cd,
   concept_path,
@@ -194,6 +164,32 @@ insert into &i2b2_data..CONCEPT_DIMENSION(
 select distinct
   ib.c_basecode,
   ib.c_dimcode,
+  max(ib.c_name),
+  max(update_date),
+  max(download_date),
+  sysdate,
+  sourcesystem_cd
+from (select * from &i2b2_metadata..PCORNET_LAB) ib
+where ib.c_basecode is not null
+group by
+  ib.c_basecode,
+  ib.c_dimcode,
+  sysdate,
+  sourcesystem_cd
+;
+
+insert into &i2b2_data..CONCEPT_DIMENSION(
+  concept_cd,
+  concept_path,
+  name_char,
+  update_date,
+  download_date,
+  import_date,
+  sourcesystem_cd
+)
+select distinct
+  ib.c_basecode,
+  ib.c_fullname,
   max(ib.c_name),
   update_date,
   download_date,
@@ -210,7 +206,6 @@ group by
   sourcesystem_cd
 ;
 
--- PK conflict
 insert into &i2b2_data..CONCEPT_DIMENSION(
   concept_cd,
   concept_path,
@@ -222,7 +217,7 @@ insert into &i2b2_data..CONCEPT_DIMENSION(
 )
 select distinct
   ib.c_basecode,
-  ib.c_dimcode,
+  ib.c_fullname,
   max(ib.c_name),
   update_date,
   download_date,
