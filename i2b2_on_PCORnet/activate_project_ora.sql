3a8208f87677f242ab6db98a7c288acfc23101e5
nihr43 <nathanihensel@gmail.com>
2020-02-21 13:29:05 -0600

    resolve "Concept missing": grant OBFSC_SERVICE_ACCOUNT required PM roles
    Without these two roles, the OBFSC service account is unable to find the
    project, and therefore the concepts associated with it.
    
    This results in the messages:
    
    `ERROR: ERROR` in the webclient when a query is ran
    
    and in the console:
    
    ```
    Matching PM response's project  [REDCap_3] with the request  project [PCORI]
    Matching PM response's project  [REDCap_1] with the request  project [PCORI]
    USER_INVALID or PM_SERVICE_PROBLEM
    ...
    Job Exception: Concept missing
    ```
    
    These messages can be seen with `log4j.logger.edu.harvard.i2b2=DEBUG` enabled
    in `standalone/deployments/i2b2.war/WEB-INF/classes/log4j.properties`

--- i2b2_on_PCORnet/activate_project_ora.sql
+++ i2b2_on_PCORnet/activate_project_ora.sql
@@ -6,6 +6,34 @@ delete from &pm_schema..pm_project_data where
   project_id = 'PCORI'
 ;
 
+-- avoid "Concept missing"
+
+insert into &pm_schema..pm_project_user_roles (
+  project_id,
+  user_id,
+  user_role_cd,
+  status_cd
+) values (
+  'PCORI',
+  'OBFSC_SERVICE_ACCOUNT',
+  'USER',
+  'A'
+);
+
+insert into &pm_schema..pm_project_user_roles (
+  project_id,
+  user_id,
+  user_role_cd,
+  status_cd
+) values (
+  'PCORI',
+  'OBFSC_SERVICE_ACCOUNT',
+  'DATA_OBFSC',
+  'A'
+);
+
+-- create project
+
 whenever sqlerror exit sql.sqlcode;
 insert into &pm_schema..pm_project_data (
   project_id,
